27582e9c8a304344105fd51bf71f7d26f8183fc4
root <root@2mgw.l.time4vps.cloud>
2020-02-27 20:32:12 +0200

    Working version of print to player console - 2

--- main.cpp
+++ main.cpp
@@ -13,16 +13,16 @@
 pluginres_t* g_result = NULL;
 //Plugin info
 plugininfo_t g_plugininfo = {
-	"netstatus",					//Plugin name
+	"Netstatus",					//Plugin name
 	Plugin_Base_version,			//version
-	"Checks network settings",				//description
-	Plugin_Base_Builder,			//author --- devon
-	"esljka.com",	//site
+	"Netstatus for JASS",				//description
+	Plugin_Base_Builder,			//author
+	"devon",	//site
 	1,								//can plugin be paused?
 	1,								//can plugin be loaded via cmd?
 	1,								//can plugin be unloaded via cmd?
 	JASS_PIFV_MAJOR,				//interface version (inside the jassapi.h)
-	JASS_PIFV_MINOR				
+	JASS_PIFV_MINOR
 };
 eng_syscall_t g_syscall = NULL;
 mod_vmMain_t g_vmMain = NULL;
@@ -34,123 +34,74 @@ C_DLLEXPORT void JASS_Query(plugininfo_t** pinfo) {
 	JASS_GIVE_PINFO();
 }
 
-//Second function called by JAZZ
-//All the code, that must be executed on start, must be written here.
-// engfunc - pointer to engine's syscall
-// modfunc - pointer to mod's vmMain
-// presult - pointer to plugin result
-// iscmd - method of loading: 0 = loaded via jass.ini, 1 = loaded via "jass load" cmd
+
 C_DLLEXPORT int JASS_Attach(eng_syscall_t engfunc, mod_vmMain_t modfunc, pluginres_t* presult, pluginfuncs_t* pluginfuncs, int iscmd) {
 	JASS_SAVE_VARS();
+
 	iscmd = 0;
 
 	return 1;
 	//return 0; is failure
 }
 
-//Last function called by JAZZ
-//All the code, that must be executed on exit, must be written here.
-// iscmd - method of unloading: 0 = loaded on server shutdown, 1 = loaded via "jass unload" cmd
+
 C_DLLEXPORT void JASS_Detach(int iscmd) {
 	iscmd = 0;
 }
 
 //This function calls before mod's vmMain (Engine - mod)
 C_DLLEXPORT int JASS_vmMain(int cmd, int arg0, int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10, int arg11) {
-	
-	/*if (cmd == GAME_CLIENT_COMMAND) { //Checking the client cmd
+	/*
+	if (cmd == GAME_CLIENT_COMMAND) { //Checking the client cmd
 		//Use g_syscall to send commands to Engine
 		//ex.: g_syscall(G_PRINT, "Hi!\n");
 		//Use g_vmMain to send commands to Mod
 		//List of possible cmd values is inside g_public.h
-	} //else if (cmd == GAME_SERVER_COMMAND){
-		//JASS_RET_SUPERCEDE(1); //command will be blocked
+	} else if (cmd == GAME_SERVER_COMMAND){
+		JASS_RET_SUPERCEDE(1); //command will be blocked
 	}
-	*/
+*/
 //g_syscall(G_PRINT, JASS_VARARGS("vmMain(%s)\n", JASS_MODMSGNAME(cmd)));
-	 if (cmd == GAME_CLIENT_COMMAND) {
+	/*if (cmd == netstatus) {
+		char* chat = JASS_VARARGS("chat " % s\n"", Q_strcat(status, sizeof(status), "score ping rate   fps packets timeNudge snaps R id name \n");
+		Q_strcat(status, sizeof(status), "----- ---- ------ --- ------- --------- ----- - -- ---------------\n");
+		g_syscall(G_SEND_SERVER_COMMAND, -1, chat);
+	}*/
+if (cmd == GAME_CLIENT_COMMAND) {
 #define strcasecmp stricmp
-		int argc = g_syscall(G_ARGC);
-		g_syscall(G_ARGV, 0, GAME_CLIENT_COMMAND);
-		if (GAME_CLIENT_COMMAND , "netstatus") {
-			char tmparg[1024];
-			g_syscall(G_PRINT, "score ping rate   fps packets timeNudge snaps R id name \n");
-			JASS_RET_IGNORED(1);
-			//G_PRINT("Netstatus");
-			//args
-		}
-	}
-	
-	if (cmd == GAME_CONSOLE_COMMAND) {
-#define strcasecmp stricmp
-		int argc = g_syscall(G_ARGC);
-		g_syscall(G_ARGV, 0, ConsoleCommand);
-		if (ConsoleCommand, "netstatus") {
-			char tmparg[1024];
 
-			g_syscall(G_PRINT, "score ping rate   fps packets timeNudge snaps R id name \n");
-			JASS_RET_IGNORED(1);
-			//G_PRINT("Netstatus");
-			//args
-		}
-	}
-	
-		if (cmd == GAME_CLIENT_COMMAND) {
-#define strcasecmp stricmp
 		int argc = g_syscall(G_ARGC);
 		char command[16];
 
 		g_syscall(G_ARGV, 0, command, sizeof(command));
 		if (!strcasecmp(command, "netstatus")) {
-
-			g_syscall(G_PRINT, "score ping rate   fps packets timeNudge snaps R id name \n");
-			JASS_RET_IGNORED(1); //command will be passed
+		g_syscall(G_SEND_SERVER_COMMAND, -1,"print  \"score ping  rate   fps packets tn    R     id   name\n\"");
+		g_syscall(G_SEND_SERVER_COMMAND, -1,"print  \"----- ---- ------- --- ------- --- ------ ----- -----\n\"");
+//g_syscall(G_SEND_SERVER_COMMAND, clientNum, "print \"What_you_want.\n\"");
+		//	g_syscall(G_PRINT, "score ping rate   fps packets timeNudge snaps R id name \n");
 		}
-		
-		
+
+
+
+
+
 	}
-
-		if (cmd == GAME_CONSOLE_COMMAND) {
-		#define strcasecmp stricmp
-		int argc = g_syscall(G_ARGC);
-		char command[16];
-		g_syscall(G_ARGV, 0, command, sizeof(command));
-		if (!strcasecmp(command, "netstatus"))
-
-		g_syscall(G_PRINT, "score ping rate   fps packets timeNudge snaps R id name \n");
-		JASS_RET_IGNORED(1); //command will be passed
-						}
-					
-				}
-				
-				
+	JASS_RET_IGNORED(1); //command will be passed
 			
-		
 	
-
-			
+}
 
 //This function calls before engine's suscall (mod - engine)
 C_DLLEXPORT int JASS_syscall(int cmd, int arg0, int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10, int arg11, int arg12) {
-/*	
+	/*
 	if (cmd == G_PRINT) {
 		JASS_RET_SUPERCEDE(1);
 	}
+*/
+//g_syscall(G_PRINT, JASS_VARARGS("syscall(%s)\n", JASS_ENGMSGNAME(cmd)));
 
-	//g_syscall(G_PRINT, JASS_VARARGS("syscall(%s)\n", JASS_ENGMSGNAME(cmd)));
-	if (cmd == GAME_CLIENT_COMMAND) {
-#define strcasecmp stricmp
-		int argc = g_syscall(G_ARGC);
-		char command[16];
-
-		g_syscall(G_ARGV, 0, command, sizeof(command));
-		if (!strcasecmp(command, "netstatus")) {
-
-			g_syscall(G_PRINT, "score ping rate   fps packets timeNudge snaps R id name \n"); */
-			JASS_RET_IGNORED(1); //command will be passed
-		
+	JASS_RET_IGNORED(1);
 }
-
 
 //This function calls after mod's vmMain (Engine - mod)
 C_DLLEXPORT int JASS_vmMain_Post(int cmd, int arg0, int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10, int arg11) {
@@ -162,11 +113,4 @@ C_DLLEXPORT int JASS_vmMain_Post(int cmd, int arg0, int arg1, int arg2, int arg3
 C_DLLEXPORT int JASS_syscall_Post(int cmd, int arg0, int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10, int arg11, int arg12) {
 	//g_syscall(G_PRINT, JASS_VARARGS("syscall_Post(%s)\n", JASS_ENGMSGNAME(cmd)));
 	JASS_RET_IGNORED(1);
-}
-
-//This function calls when other plugin or JASS by itself onvokes it
-//Place there code that will be shared to other plugins
-C_DLLEXPORT int JASS_pluginCall(int cmd, int arg0, int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10, int arg11, int arg12) {
-	return 0;
-	
 }
